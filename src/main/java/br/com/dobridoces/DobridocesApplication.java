c921b03e5a74307028b8329d2a55d61502d2890a
Felipe <felipe@amee.com.br>
2020-01-08 10:43:05 -0300

    Commit Inicial

---
+++ src/main/java/br/com/dobridoces/DobridocesApplication.java
@@ -0,0 +1,13 @@
+package br.com.dobridoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobridocesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobridocesApplication.class, args);
+	}
+
+}
